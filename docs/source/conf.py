745f8381dd2b2a5473942d3f7878a6054b36865b
Lirry18 <lirry.pinter@gmail.com>
2021-12-02 11:21:48 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Elementa'
+copyright = '2021, Elementa'
+author = 'Lirry'
 
 release = '0.1'
 version = '0.1.0'
